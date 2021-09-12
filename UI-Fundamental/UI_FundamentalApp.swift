713f8f988070a1435c000a3cab0657a80020ac3a
Vong Nyuk Soon <vongnyuksoon2000@gmail.com>
2021-09-12 16:03:56 +0800

    Initial Commit

---
+++ UI-Fundamental/UI_FundamentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_FundamentalApp.swift
+//  UI-Fundamental
+//
+//  Created by Vong Nyuksoon on 12/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct UI_FundamentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
